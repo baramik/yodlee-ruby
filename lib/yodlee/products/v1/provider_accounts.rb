013a940b0686aa578dbf4a8c793394c14d5fa4b5
dmitriybaramikov <baramik1989@gmail.com>
2021-11-14 19:34:05 +0200

    Fix undefined var

--- lib/yodlee/products/v1/provider_accounts.rb
+++ lib/yodlee/products/v1/provider_accounts.rb
@@ -2,7 +2,7 @@ module Yodlee
   module V1
     class ProviderAccounts < Yodlee::BaseProduct
       def get(provider_account_id, auth_token:)
-        raise 'provider account id could not be nil' unless id
+        raise 'provider account id could not be nil' unless provider_account_id
 
         http_client.get('providerAccounts/' << provider_account_id.to_s, auth_token: auth_token)
       end
